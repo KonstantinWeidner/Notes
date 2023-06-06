05a8c7eeb83c9802f9de532d637d760a26190fe9
Konstantin <konstantin.weidner99@gmail.com>
2023-06-06 11:26:39 +0200

    button is now working :)

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.notes;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,29 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        notes.add("Welcome!");
-        notes.add("2");
-        notes.add("3");
-
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
-
-                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class); //jump to editor
-                intent.putExtra("noteID", i);
-                startActivity(intent);
-            }
-        });
-
         Button button = (Button)findViewById(R.id.addNote);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-            }
+                notes.add("");
+                arrayAdapter.notifyDataSetChanged();            }
         });
+
+        notes.add("Welcome!");
+
+
+            arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+            listView.setAdapter(arrayAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+
+                    Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class); //jump to editor
+                    intent.putExtra("noteID", i);
+                    startActivity(intent);
+                }
+            });
     }
 }
